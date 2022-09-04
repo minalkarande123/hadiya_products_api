6e4a60dfa0267a2203d479a2a8add279e2192ac4
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2022-09-04 19:28:17 +0530

    renamed sequelize config file

--- src/database/models/index.cjs
+++ src/database/models/index.cjs
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 // console.log(env);
-const config = require('../../config/config.json')[env];
+const config = require('../../config/config.cjs')[env];
 // console.log(config);
 const db = {};
 // console.log("Hello");
