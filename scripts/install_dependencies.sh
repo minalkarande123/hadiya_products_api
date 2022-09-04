c984af0ec4b4ffb7fed4192249c12dec24cc27a7
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2022-09-04 18:53:29 +0530

    changes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ ! -d /home/ubuntu/hadiya/products ]; then
-    echo "Creating directory /home/ubuntu/hadiya/products ..."
-    mkdir -p /home/ubuntu/hadiya/products
+    echo "Project directory doesn't exists. Exiting with code 1 ...";
+    exit 1;
 fi
 cd /home/ubuntu/hadiya/products
 echo "Installing dependencies ..."
-npm install
+npm install --omit=dev
 echo "Installation complete!"
